1e02809ab19c5a06c7b657bc95aff4fdbdc691d7
Zenish Shrestha <zenishshr@gmail.com>
2020-05-21 14:12:31 +0545

    Delete Sites

--- Sites/php-esewa/resources/views/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-@section('content')
-<h1>Buy something!</h1>
-        <form action="{{ route('process-payment') }}" method="POST">
-            @csrf
-            <input value="Pay with Esewa NRS.10 only." class="btn btn-dark" type="submit">
-        </form>
-@endsection
